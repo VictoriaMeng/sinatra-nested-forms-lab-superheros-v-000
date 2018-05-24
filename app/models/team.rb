3013ac78b78c1084036e07290a0249961488deea
Victoria Meng <nights1ststar@protonmail.com>
2018-05-24 18:47:08 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,14 @@
-class Team 
+class Team
+  attr_accessor :name, :motto, :heroes
+
+  @@all = []
+
   def initialize(args)
     args.each { |key, value| self.send("#{key}=", value) }
     @@all << self
   end
-  
+
+  def self.all
+    @@all
+  end
 end
